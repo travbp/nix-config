935e0fd4452a8889855e00d8ede0b08b548885d1
Travis Peter <travbpeter@gmail.com>
2024-10-05 23:00:50 -0500

    added adguard

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
+      ./modules/adguard.nix
     ];
 
   # Bootloader.
@@ -70,18 +67,9 @@
     adguardhome
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
   services = {
     tailscale.enable = true;
-    adguardhome.enable = true;
   };
   
   systemd.services.tailscaled.after=["NetworkManager-wait-online.service"];
@@ -114,18 +102,6 @@
     '';
   };
 
-
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-
   networking.firewall = {
     # enable the firewall
     enable = true;
@@ -138,13 +114,6 @@
 
   };
 
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
   nix = {
---
+++ modules/adguard.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, ... }: {
+  config = {
+    networking = {
+      firewall.interfaces.tailscale0 = {
+        allowedTCPPorts = [
+          3000 
+          853
+        ];
+        allowedUDPPorts = [
+          # 53
+          853
+        ];
+      };
+    };
+
+    services = {
+      adguardhome = {
+        enable = true;
+      };
+    };
+  };
+}
