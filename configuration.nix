6f27ce4b860d5227382e0ddef96324a40fda3f33
Travis Peter <travbpeter@gmail.com>
2025-04-05 16:49:42 -0500

    2025 refresh

--- configuration.nix
+++ configuration.nix
@@ -10,17 +10,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
 
   networking.hostName = "nixos-prod"; # Define your hostname.
-  networking.domain = "travis-peter.com";
-  networking.nameservers = [
-    "9.9.9.9"
-    "149.112.112.112"
-  ];
-
-  # disable power management to hopefully prevent sleeping
-  powerManagement.enable = false;
+  # networking.domain = "travis-peter.com";
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
@@ -39,16 +32,6 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-
-  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-  # If no user is logged in, the machine will power down after 20 minutes.
-  
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.travis = {
@@ -73,39 +56,8 @@
   # List services that you want to enable:
   services = {
     tailscale.enable = true;
-    adguardhome.enable = true;
   };
   
-  systemd.services.tailscaled.after=["NetworkManager-wait-online.service"];
-
-  # create a oneshot job to authenticate to Tailscale
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-  
-    # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-  
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up --ssh --auth-key=
-    '';
-  };
-
   networking.firewall = {
     # enable the firewall
     enable = true;
@@ -116,41 +68,8 @@
     # allow the Tailscale UDP port through the firewall
     allowedUDPPorts = [ config.services.tailscale.port ];
 
-    # adguard
-    interfaces.tailscale0 = {
-        allowedTCPPorts = [
-          3000
-          80
-          53
-          853
-        ];
-        allowedUDPPorts = [
-          53
-          853
-        ];
-      };
   };
 
-  services.adguardhome.settings = {
-    auth_attempts = 3;
-    block_auth_min = 10;
-    http.address = "127.0.0.1:3000";
-
-    dns = {
-      protection_enabled = true;
-      parental_enabled = false;
-      bind_hosts = [ "0.0.0.0" ];
-    };
-    statistics = {
-      enabled = true;
-      interval = "8760h";
-    };
-    dhcp.enabled = false;
-  };
-
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = "experimental-features = nix-command flakes";
-  };
+  nix.settings.experimental-features = ["nix-command" "flakes" ];
 
 }
